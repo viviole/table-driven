d29ec60b12ccab06c2d896b2f84bdab38a7d5e79
Matthew Theodore Lempas <theodorelempas@gmail.com>
2024-03-12 21:22:08 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302223116/Program.cs
+++ tpmodul4_1302223116/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    internal class kelurahan
+    {
+        public enum Kelurahan
+        {
+            Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Kebonwaru, Maleer, Samoja
+        };
+
+        public static int getKodePos(Kelurahan kelurahan)
+        {
+            int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+            return kodePos[(int)kelurahan];
+        }
+    }
+}
+
